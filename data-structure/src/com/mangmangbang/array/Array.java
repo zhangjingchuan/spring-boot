94f57f69dfa710a4af589bd548667b3599873900
张景川 <64677664@qq.com>
2019-12-03 17:05:36 +0800

    添加元素

--- data-structure/src/com/mangmangbang/array/Array.java
+++ data-structure/src/com/mangmangbang/array/Array.java
@@ -1,5 +1,7 @@
 package com.mangmangbang.array;
 
+import java.security.SignatureException;
+
 /**
  * created by zhangjingchuan on 2019/12/3
  */
@@ -48,6 +50,42 @@ public class Array {
         return size == 0;
     }
 
+    /**
+     * 向所有元素后添加一个新元素
+     * @param e
+     */
+    public void addLast(int e){
+        this.add(size,e);
+    }
 
+    /**
+     * 在第index个位置插入一个新元素e
+     * @param index
+     * @param e
+     */
+    public void add(int index,int e){
+        if(size == data.length){
+            throw new IllegalArgumentException("AddLast failed. Array is full.");
+        }
+
+        if(index < 0 || index > size){
+            throw new IllegalArgumentException("AddLast failed. Require index >=0 and index <=size");
+        }
+
+        for(int i = size - 1;i >= size ; i--){
+            data[i+1] = data[i];
+        }
+
+        data[index] = e;
+        size++;
+    }
+
+    /**
+     * 在第一个位置插入一个新元素e
+     * @param e
+     */
+    public void addFirst(int e){
+        this.add(0,e);
+    }
 
 }
