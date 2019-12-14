331c7f4a3d50889cd8e836df456cae0543ed2ec3
zhangjingchuan <64677664@qq.com>
2019-12-14 11:33:13 +0800

    二叉搜索树完善

--- data-structure/src/com/mangmangbang/bst/BST.java
+++ data-structure/src/com/mangmangbang/bst/BST.java
@@ -45,45 +45,113 @@ public class BST<E extends Comparable<E>> {
      * @param e
      */
     public void add(E e){
-        if(root == null){
-            root = new Node(e);
-            size++;
-
-        }else{
-            add(root,e);
-        }
-
-
-
+        root = add(root,e);
     }
 
     /**
      * 向以node为根的二分搜索树中插入元素E
-     *
      * 递归算法
      * @param node
      * @param e
+     * @return 返回插入新节点后二分搜索树的根
      */
-    private void add(Node node,E e){
-        if(node.e.equals(e)){
-            return ;
+    private Node add(Node node,E e){
+        if(node == null){
+            size ++;
+            return new Node(e);
         }
-        if(node.e.compareTo(e)<0&&node.left == null){
-            node.left = new Node(e);
-            size++;
-            return ;
+
+        if(node.e.compareTo(e)<0){
+            node.right = add(node.right,e);
+        }else if(node.e.compareTo(e)>0){
+            node.left = add(node.left,e);
         }
-        if(node.e.compareTo(e)>0&&node.right == null){
-            node.right = new Node(e);
-            size++;
-            return ;
+
+        return node;
+    }
+
+    /**
+     * 查看二分搜索树中是否包含元素e
+     * @param e
+     * @return
+     */
+    public boolean contains(E e){
+        return this.contains(root,e);
+    }
+
+    private boolean contains(Node node,E e){
+        if(node == null){
+            return false;
         }
 
         if(node.e.compareTo(e)>0){
-            add(node.right,e);
-        }else{
-            add(node.left,e);
+            return contains(root.right,e);
         }
+
+        if(node.e.compareTo(e)<0){
+            return contains(root.left,e);
+        }
+
+        if(node.e.equals(e)){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 二分搜索树的前序遍历
+     */
+    public void preOrder(){
+        preOrder(root);
+    }
+
+    /**
+     * 前序遍历以node为根的二分搜索树，递归算法
+     * @param node
+     */
+    private void preOrder(Node node){
+        if(node == null){
+            return ;
+        }
+
+        System.out.println(node.e);
+
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+
+        generateBSTString(root,0,res);
+
+        return res.toString();
+    }
+
+    /**
+     * 生成以node为根节点，深度为depth的描述二叉树的字符串
+     * @param node
+     * @param depth
+     * @param res
+     */
+    private void generateBSTString(Node node, int depth, StringBuilder res) {
+        if(node == null){
+            res.append(generateDepthString(depth)+"null\n");
+            return ;
+        }
+
+        res.append(generateDepthString(depth) + node.e + "\n");
+        generateBSTString(node.left , depth+1 ,res);
+        generateBSTString(node.right , depth+1 ,res);
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for(int i=0;i<depth;i++){
+            res.append("--");
+        }
+        return res.toString();
     }
 }
 
